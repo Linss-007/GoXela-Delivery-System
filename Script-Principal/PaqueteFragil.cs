993e0006abd773c4b023338289785528a581a08b
Samir1984 <allandanielito1984@gmail.com>
2026-08-31 12:53:45 -0600

    Feat(PaqueteFragil): Metodo consctuctor de Paquete Fragil

---
+++ Script-Principal/PaqueteFragil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Script_Principal
+{
+    internal class PaqueteFragil: Paquetes
+    {
+		private bool RequiereManejoEspecial;
+
+		public bool requiereManejoEspecial
+		{
+			get { return RequiereManejoEspecial; }
+			set { RequiereManejoEspecial = value; }
+		}
+		public PaqueteFragil(string codigoIng, string descripIng, double pesoIng, double valorIng, string direcOrigenIng, string direcDestinoIng, string estadoIng, bool requiereManejoEspecial) : base(codigoIng, descripIng, pesoIng, valorIng, direcOrigenIng, direcDestinoIng, estadoIng )
+		{
+			RequiereManejoEspecial = requiereManejoEspecial;
+		}
+
+	}
+}
