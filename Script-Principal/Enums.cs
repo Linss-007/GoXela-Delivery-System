d1fa4fc189c2894e182519b30964e699ce2df4c3
Samir1984 <allandanielito1984@gmail.com>
2026-08-30 20:18:56 -0600

    feat(enums): agregado archivo de enumeraciones para estados y tipos

---
+++ Script-Principal/Enums.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Script_Principal
+{
+    public enum EstadoRepartidor
+    {
+        Disponible,
+        Asignado,
+        FueraDeServicio
+    }
+    public enum EstadoVehiculo
+    {
+        Disponible,
+        Asignado,
+        EnMantenimiento
+    }
+
+    public enum EstadoPaquete
+    {
+        Registrado,
+        Asignado,
+        EnTransito,
+        Entregado
+    }
+    public enum EstadoEntrega
+    {
+        Solicitada,
+        Asignada,
+        Recogida,
+        EnRuta,
+        Entregada,
+        Cancelada,
+        Reprogramada,
+        Incidencia
+    }
+    public enum TipoServicio
+    {
+        Normal,
+        Prioritario,
+        Urgente
+    }
+    public enum TipoIncidencia
+    {
+        ClienteAusente,
+        DireccionIncorrecta,
+        PaqueteDañado,
+        VehiculoAveriado,
+        Retraso,
+        ProblemasClimaticos,
+        RechazoRecepcion
+    }
+}
