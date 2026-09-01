f9ba4b91629a9b97a9ccc7cf6ad7ff535889ccda
Samir1984 <allandanielito1984@gmail.com>
2026-08-31 21:52:39 -0600

    Fix(DevMain): Llamada del metodo Gestionamiento de Vehículo al case correcto del menú principal

--- Script-Principal/Program.cs
+++ Script-Principal/Program.cs
@@ -319,16 +319,16 @@ namespace Script_Principal
                         break;
                     case 2:
                         Console.Clear();
-                        GestionarVehiculos();
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine("+===============================================+");
+                        Console.WriteLine("|            Gestión de Repartidores            |");
+                        Console.WriteLine("+===============================================+");
+                        Console.ResetColor();
                         Console.ReadLine();
                         break;
                     case 3:
                         Console.Clear();
-                        Console.ForegroundColor = ConsoleColor.DarkYellow;
-                        Console.WriteLine("+===========================================+");
-                        Console.WriteLine("|            Gestión de Vehículos           |");
-                        Console.WriteLine("+===========================================+");
-                        Console.ResetColor();
+                        GestionarVehiculos();
                         Console.ReadLine();
                         break;
                     case 4:
