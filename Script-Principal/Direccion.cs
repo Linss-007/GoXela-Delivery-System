9232e3ed95655b1972c0b6c9e3f2df4c5231ec74
Samir1984 <allandanielito1984@gmail.com>
2026-09-05 10:23:51 -0600

    feat(Direccion):Creacion de la primera structura

---
+++ Script-Principal/Direccion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Script_Principal
+{
+    public struct Direccion
+    {
+        public string Calle;
+        public string Referencia;
+
+        public Direccion(string calle, string referencia)
+        {
+            Calle = calle;
+            Referencia = referencia;
+        }
+        public override string ToString()
+        {
+            return $"{Calle} ({Referencia})";
+        }
+        public int LongitudTotal()
+        {
+            return (Calle ?? "").Length + (Referencia ?? "").Length;
+        }
+    }
+}
